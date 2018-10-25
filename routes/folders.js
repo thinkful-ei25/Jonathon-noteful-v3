0f72facb688f30d9c29e13a81c2862b2c350e952
Reldalk <Reldalk@gmail.com>
2018-10-25 16:29:21 -0400

    finished endpoints

---
+++ routes/folders.js
@@ -0,0 +1,147 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Folder = require('../models/folder');
+
+const router = express.Router();
+
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  let filter = {};
+
+  if (searchTerm) {
+    filter.title = { $regex: searchTerm, $options: 'i' };
+
+    // Mini-Challenge: Search both `title` and `content`
+    // const re = new RegExp(searchTerm, 'i');
+    // filter.$or = [{ 'title': re }, { 'content': re }];
+  }
+
+  Folder.find(filter)
+    .sort({ name: 'asc' })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+  Folder.findById(id)
+    .then(result => {
+      if(result) {
+        res.json(result);
+      }else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Unable to find any items');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const {name} = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newNote = {name};
+
+  Folder.create(newNote)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The folder name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (!name) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateNote = { name };
+
+  Folder.findByIdAndUpdate(id, updateNote, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Folder does not exist');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Folder.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Folder does not exist');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+
+
+module.exports = router;
